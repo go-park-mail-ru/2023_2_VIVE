65fcfce931bc0fcabb77b5ac61db0c4bee48b0b0
Vladimir Borozenets <borozenets2002@mail.ru>
2023-09-28 22:50:33 +0300

    VIVE-33: Добавлен дополнительный функционал к User

--- models/user.go
+++ models/user.go
@@ -1,5 +1,12 @@
 package models
 
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+	"sync"
+)
+
 type Role string
 
 const (
@@ -14,4 +21,40 @@ type User struct {
 	FirstName string `json:"first_name,omitempty"`
 	LastName  string `json:"last_name,omitempty"`
 	Type      Role   `json:"role,omitempty"`
+}
+
+var users = &sync.Map{}
+
+func CheckPassword(user User) error {
+	hasher := sha256.New()
+	hasher.Write([]byte(user.Password))
+	hashedPass := hasher.Sum(nil)
+
+	actualPass, ok := users.Load(user.Email)
+
+	if !ok {
+		return fmt.Errorf("User not found")
+	}
+
+	if hex.EncodeToString(hashedPass) != hex.EncodeToString(actualPass.([]byte)) {
+		return fmt.Errorf("Password is incorrect")
+	}
+
+	return nil
+}
+
+func AddUser(user User) error {
+	_, exist := users.Load(user.Email)
+
+	if exist {
+		return fmt.Errorf("Email already exists")
+	}
+
+	hasher := sha256.New()
+	hasher.Write([]byte(user.Password))
+	hashedPass := hasher.Sum(nil)
+
+	users.Store(user.Email, hashedPass)
+
+	return nil
 }
