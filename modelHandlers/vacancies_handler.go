13cef361041b1371811a253b09a6a3618746b7bd
Vladimir Borozenets <borozenets2002@mail.ru>
2023-09-30 21:15:15 +0300

    VIVE-32: Инициализировать лист вакансий на старте

--- modelHandlers/vacancies_handler.go
+++ modelHandlers/vacancies_handler.go
@@ -3,8 +3,6 @@ package modelHandlers
 import "models/models"
 
 func GetVacancies() []models.Vacancy {
-	models.CreateVacancies()
-
 	defer models.Vac.Mu.Unlock()
 
 	models.Vac.Mu.Lock()
--- models/vacancy.go
+++ models/vacancy.go
@@ -16,45 +16,42 @@ type vacancies struct {
 }
 
 var Vac = vacancies{
-	VacancyList: make([]Vacancy, 5),
-	Mu:          &sync.Mutex{},
-}
-
-func CreateVacancies() {
-	Vac.VacancyList = append(Vac.VacancyList,
-		Vacancy{
+	VacancyList: []Vacancy{
+		{
 			ID:          1,
 			Name:        "C++ developer",
 			CompanyName: "VK",
 			Description: "Middle C++ developer in Mail.ru team",
 			Salary:      2500,
 		},
-		Vacancy{
+		{
 			ID:          2,
 			Name:        "Go developer",
 			CompanyName: "VK",
 			Description: "Golang junior developer without any experience",
 			Salary:      1000,
 		},
-		Vacancy{
+		{
 			ID:          3,
 			Name:        "HR",
 			CompanyName: "Yandex",
 			Description: "Human resources specialist",
 			Salary:      700,
 		},
-		Vacancy{
+		{
 			ID:          4,
 			Name:        "Frontend developer",
 			CompanyName: "Google",
 			Description: "Middle Frontend developer, JavaScript, HTML, Figma",
 			Salary:      5000,
 		},
-		Vacancy{
+		{
 			ID:          5,
 			Name:        "Project Manager",
 			CompanyName: "VK",
 			Description: "Experienced specialist in IT-management",
 			Salary:      2000,
-		})
+		}},
+
+	Mu: &sync.Mutex{},
 }
