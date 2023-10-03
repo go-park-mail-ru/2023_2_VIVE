7822fabf4bf56124b8b710df86cb67ace03a2553
Vladimir Borozenets <borozenets2002@mail.ru>
2023-10-03 14:59:39 +0300

    VIVE-46: Добавить CORS в настройки конфигурации

--- configs/server_configs.go
+++ configs/server_configs.go
@@ -1,5 +1,13 @@
 package configs
 
+import "github.com/rs/cors"
+
 const (
 	PORT = ":8081"
 )
+
+var CORS = cors.New(cors.Options{
+	AllowedOrigins:   []string{"http://212.233.90.231:8082"},
+	AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE"},
+	AllowCredentials: true,
+})
--- go.mod
+++ go.mod
@@ -11,5 +11,6 @@ require (
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/rs/cors v1.10.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
