f625aeb185e0153a9871eb160e3ecd7043db77ba
Vladimir Borozenets <borozenets2002@mail.ru>
2023-10-06 15:57:49 +0300

    VIVE: Добавить валидацию роли при логине

--- serverErrors/internal_errors.go
+++ serverErrors/internal_errors.go
@@ -7,6 +7,7 @@ var (
 	INVALID_EMAIL          = fmt.Errorf("The entered email-address is not a real one")
 	INVALID_PASSWORD       = fmt.Errorf("The entered password does not meet the requirements")
 	INVALID_ROLE           = fmt.Errorf("The entered role does not exist")
+	INCORRECT_ROLE         = fmt.Errorf("An account with chosen role does not exist")
 	NO_DATA_FOUND          = fmt.Errorf("Account data not found")
 	ACCOUNT_ALREADY_EXISTS = fmt.Errorf("An account with given email already exists")
 	SESSION_ALREADY_EXISTS = fmt.Errorf("Session has already started")
