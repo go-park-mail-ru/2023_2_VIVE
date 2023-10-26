0e0078c77e572f53e833c5b7ddedb558e0f89336
Vladimir Konoplyuk <konoplyuk.v@yandex.ru>
2023-10-27 00:36:15 +0300

    VIVE-58: Изменить тип первичного ключа в отношениях на serial

--- db/migrations/001_initial_migration.sql
+++ db/migrations/001_initial_migration.sql
@@ -1,157 +1,169 @@
-CREATE SCHEMA hnh_data AUTHORIZATION vive_admin;
+CREATE SCHEMA hnh_data AUTHORIZATION vovchenskiy;
 
 CREATE TABLE user_profile (
-	id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
-	email CHARACTER(256) NOT NULL UNIQUE,
-	pswd bytea NOT NULL,
-	salt bytea NOT NULL UNIQUE,
-	first_name TEXT NOT NULL
-		CONSTRAINT first_name_is_not_empty CHECK (length(first_name) > 0),
-	last_name TEXT NOT NULL
-		CONSTRAINT last_name_is_not_empty CHECK (length(last_name) > 0),
-	birthday date DEFAULT NULL,
-	phone_number CHARACTER(16) DEFAULT NULL
-		CONSTRAINT phone_number_is_not_empty CHECK (length(phone_number) > 0),
-	location TEXT DEFAULT NULL
-		CONSTRAINT location_is_not_empty CHECK (length(location) > 0)
+    id serial PRIMARY KEY
+        CONSTRAINT id_is_positive CHECK (id > 0),
+    email CHARACTER(256) NOT NULL UNIQUE,
+    pswd bytea NOT NULL,
+    salt bytea NOT NULL UNIQUE,
+    first_name TEXT NOT NULL
+        CONSTRAINT first_name_is_not_empty CHECK (length(first_name) > 0),
+    last_name TEXT NOT NULL
+        CONSTRAINT last_name_is_not_empty CHECK (length(last_name) > 0),
+    birthday date DEFAULT NULL,
+    phone_number CHARACTER(16) DEFAULT NULL
+        CONSTRAINT phone_number_is_not_empty CHECK (length(phone_number) > 0),
+    location TEXT DEFAULT NULL
+        CONSTRAINT location_is_not_empty CHECK (length(location) > 0)
 );
 
 CREATE TABLE employer (
-	id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
-	user_id uuid REFERENCES user_profile ON DELETE CASCADE 
+    id serial PRIMARY KEY
+        CONSTRAINT id_is_positive CHECK (id > 0),
+    user_id int REFERENCES user_profile ON DELETE CASCADE 
 );
 
 CREATE TABLE applicant (
-	id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
-	user_id uuid REFERENCES user_profile ON DELETE CASCADE,
-	status TEXT NOT NULL DEFAULT 'searching'
-		CONSTRAINT status_is_not_empty CHECK (length(status) > 0)
+    id serial PRIMARY KEY
+        CONSTRAINT id_is_positive CHECK (id > 0),
+    user_id int REFERENCES user_profile ON DELETE CASCADE,
+    status TEXT NOT NULL DEFAULT 'searching'
+        CONSTRAINT status_is_not_empty CHECK (length(status) > 0)
 );
 
 CREATE TABLE organization (
-	name TEXT PRIMARY KEY
-		CONSTRAINT name_is_not_empty CHECK (length(name) > 0),
-	employer_id uuid REFERENCES employer ON DELETE CASCADE,
-	location TEXT DEFAULT NULL
-		CONSTRAINT location_is_not_empty CHECK (length(location) > 0),
-	description TEXT NOT NULL
-		CONSTRAINT description_is_not_empty CHECK (length(description) > 0)
+    name TEXT PRIMARY KEY
+        CONSTRAINT name_is_not_empty CHECK (length(name) > 0),
+    employer_id int REFERENCES employer ON DELETE CASCADE,
+    location TEXT DEFAULT NULL
+        CONSTRAINT location_is_not_empty CHECK (length(location) > 0),
+    description TEXT NOT NULL
+        CONSTRAINT description_is_not_empty CHECK (length(description) > 0)
 );
 
 CREATE TABLE vacancy (
-	id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
-	employer_id uuid REFERENCES employer ON DELETE CASCADE,
-	name TEXT UNIQUE NOT NULL
-		CONSTRAINT name_is_not_empty CHECK (length(name) > 0),
-	description TEXT NOT NULL
-		CONSTRAINT description_is_not_empty CHECK (length(description) > 0),
-	salary_lower_bound int DEFAULT NULL
-		CONSTRAINT salary_lower_bound_is_positive CHECK (salary_lower_bound > 0),
-	salary_upper_bound int DEFAULT NULL
-		CONSTRAINT salary_upper_bound_is_positive CHECK (salary_upper_bound > 0),
-	employment TEXT DEFAULT NULL
-		CONSTRAINT employment_is_not_empty CHECK (length(employment) > 0),
-	experience_lower_bound int DEFAULT NULL
-		CONSTRAINT experience_lower_bound_is_positive CHECK (experience_lower_bound > 0),
-	experience_upper_bound int DEFAULT NULL
-		CONSTRAINT experience_upper_bound_is_positive CHECK (experience_upper_bound > 0),
-	education_type TEXT NOT NULL DEFAULT 'secondary'
-		CONSTRAINT education_type_is_not_empty CHECK (length(education_type) > 0),
-	location TEXT DEFAULT NULL
-		CONSTRAINT location_is_not_empty CHECK (length(location) > 0),
-	created_at timestamptz DEFAULT now(),
-	updated_at timestamptz DEFAULT now(),
-	CONSTRAINT valid_salaries CHECK (salary_lower_bound <= salary_upper_bound),
-	CONSTRAINT valid_experience CHECK (experience_lower_bound <= salary_upper_bound)
+    id serial PRIMARY KEY
+        CONSTRAINT id_is_positive CHECK (id > 0),
+    employer_id int REFERENCES employer ON DELETE CASCADE,
+    name TEXT UNIQUE NOT NULL
+        CONSTRAINT name_is_not_empty CHECK (length(name) > 0),
+    description TEXT NOT NULL
+        CONSTRAINT description_is_not_empty CHECK (length(description) > 0),
+    salary_lower_bound int DEFAULT NULL
+        CONSTRAINT salary_lower_bound_is_positive CHECK (salary_lower_bound > 0),
+    salary_upper_bound int DEFAULT NULL
+        CONSTRAINT salary_upper_bound_is_positive CHECK (salary_upper_bound > 0),
+    employment TEXT DEFAULT NULL
+        CONSTRAINT employment_is_not_empty CHECK (length(employment) > 0),
+    experience_lower_bound int DEFAULT NULL
+        CONSTRAINT experience_lower_bound_is_positive CHECK (experience_lower_bound > 0),
+    experience_upper_bound int DEFAULT NULL
+        CONSTRAINT experience_upper_bound_is_positive CHECK (experience_upper_bound > 0),
+    education_type TEXT NOT NULL DEFAULT 'secondary'
+        CONSTRAINT education_type_is_not_empty CHECK (length(education_type) > 0),
+    location TEXT DEFAULT NULL
+        CONSTRAINT location_is_not_empty CHECK (length(location) > 0),
+    created_at timestamptz DEFAULT now(),
+    updated_at timestamptz DEFAULT now(),
+    CONSTRAINT valid_salaries CHECK (salary_lower_bound <= salary_upper_bound),
+    CONSTRAINT valid_experience CHECK (experience_lower_bound <= salary_upper_bound)
 );
 
 CREATE TABLE cv (
-	id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
-	applicant_id uuid REFERENCES applicant ON DELETE CASCADE,
-	status TEXT NOT NULL DEFAULT 'serching'
-		CONSTRAINT status_is_not_empty CHECK (length(status) > 0),
-	created_at timestamptz DEFAULT now(),
-	updated_at timestamptz DEFAULT now()
+    id serial PRIMARY KEY
+        CONSTRAINT id_is_positive CHECK (id > 0),
+    applicant_id int REFERENCES applicant ON DELETE CASCADE,
+    status TEXT NOT NULL DEFAULT 'serching'
+        CONSTRAINT status_is_not_empty CHECK (length(status) > 0),
+    created_at timestamptz DEFAULT now(),
+    updated_at timestamptz DEFAULT now()
 );
 
 CREATE TABLE responce (
-	id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
-	vacancy_id uuid REFERENCES vacancy ON DELETE CASCADE,
-	cv_id uuid REFERENCES cv ON DELETE CASCADE,
-	created_at timestamptz DEFAULT now(),
-	updated_at timestamptz DEFAULT now()
+    id serial PRIMARY KEY
+        CONSTRAINT id_is_positive CHECK (id > 0),
+    vacancy_id int REFERENCES vacancy ON DELETE CASCADE,
+    cv_id int REFERENCES cv ON DELETE CASCADE,
+    created_at timestamptz DEFAULT now(),
+    updated_at timestamptz DEFAULT now()
 );
 
 CREATE TABLE experience (
-	id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
-	cv_id uuid REFERENCES cv ON DELETE CASCADE,
-	organization_name TEXT NOT NULL
-		CONSTRAINT organization_name_is_not_empty CHECK (length(organization_name) > 0),
-	description TEXT NOT NULL
-		CONSTRAINT description_is_not_empty CHECK (length(description) > 0),
-	start_date date NOT NULL,
-	end_date date DEFAULT NULL
+    id serial PRIMARY KEY
+        CONSTRAINT id_is_positive CHECK (id > 0),
+    cv_id int REFERENCES cv ON DELETE CASCADE,
+    organization_name TEXT NOT NULL
+        CONSTRAINT organization_name_is_not_empty CHECK (length(organization_name) > 0),
+    description TEXT NOT NULL
+        CONSTRAINT description_is_not_empty CHECK (length(description) > 0),
+    start_date date NOT NULL,
+    end_date date DEFAULT NULL
 );
 
 CREATE TABLE "language" (
-	id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
-	name TEXT NOT NULL
-		CONSTRAINT name_is_not_empty CHECK (length(name) > 0),
-	level TEXT NOT NULL
-		CONSTRAINT level_is_not_empty CHECK (length(level) > 0),
-	UNIQUE (name, level)
+    id serial PRIMARY KEY
+        CONSTRAINT id_is_positive CHECK (id > 0),
+    name TEXT NOT NULL
+        CONSTRAINT name_is_not_empty CHECK (length(name) > 0),
+    level TEXT NOT NULL
+        CONSTRAINT level_is_not_empty CHECK (length(level) > 0),
+    UNIQUE (name, level)
 );
 
 CREATE TABLE cv_language_assign (
-	cv_id uuid REFERENCES cv ON DELETE CASCADE,
-	language_id uuid REFERENCES "language" ON DELETE CASCADE,
-	PRIMARY KEY (cv_id, language_id)
+    cv_id int REFERENCES cv ON DELETE CASCADE,
+    language_id int REFERENCES "language" ON DELETE CASCADE,
+    PRIMARY KEY (cv_id, language_id)
 );
 
 CREATE TABLE education_institution (
-	id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
-	name TEXT NOT NULL
-		CONSTRAINT name_is_not_empty CHECK (length(name) > 0),
-	education_level TEXT NOT NULL
-		CONSTRAINT education_level_is_not_empty CHECK (length(education_level) > 0),
-	UNIQUE (name, education_level)
+    id serial PRIMARY KEY
+        CONSTRAINT id_is_positive CHECK (id > 0),
+    name TEXT NOT NULL
+        CONSTRAINT name_is_not_empty CHECK (length(name) > 0),
+    education_level TEXT NOT NULL
+        CONSTRAINT education_level_is_not_empty CHECK (length(education_level) > 0),
+    UNIQUE (name, education_level)
 );
 
 CREATE TABLE major_field (
-	id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
-	name TEXT NOT NULL UNIQUE
-		CONSTRAINT name_is_not_empty CHECK (length(name) > 0)
+    id serial PRIMARY KEY
+        CONSTRAINT id_is_positive CHECK (id > 0),
+    name TEXT NOT NULL UNIQUE
+        CONSTRAINT name_is_not_empty CHECK (length(name) > 0)
 );
 
 CREATE TABLE institution_major_assign (
-	id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
-	institution_id uuid REFERENCES education_institution ON DELETE CASCADE,
-	major_field_id uuid REFERENCES major_field ON DELETE CASCADE,
-	UNIQUE (institution_id, major_field_id)
+    id serial PRIMARY KEY
+        CONSTRAINT id_is_positive CHECK (id > 0),
+    institution_id int REFERENCES education_institution ON DELETE CASCADE,
+    major_field_id int REFERENCES major_field ON DELETE CASCADE,
+    UNIQUE (institution_id, major_field_id)
 );
 
 CREATE TABLE education (
-    cv_id uuid REFERENCES cv ON DELETE CASCADE,
-    institution_major_id uuid REFERENCES institution_major_assign ON DELETE CASCADE,
+    cv_id int REFERENCES cv ON DELETE CASCADE,
+    institution_major_id int REFERENCES institution_major_assign ON DELETE CASCADE,
     graduation_year CHARACTER(4) NOT NULL
         CONSTRAINT valid_graduation_year CHECK (length(graduation_year) = 4),
-	PRIMARY KEY (cv_id, institution_major_id)
+    PRIMARY KEY (cv_id, institution_major_id)
 );
 
 CREATE TABLE skill (
-    id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    id serial PRIMARY KEY
+        CONSTRAINT id_is_positive CHECK (id > 0),
     name TEXT NOT NULL UNIQUE
         CONSTRAINT name_is_not_empty CHECK (length(name) > 0)
 );
 
 CREATE TABLE cv_skill_assign (
-    cv_id uuid REFERENCES cv ON DELETE CASCADE,
-    skill_id uuid REFERENCES skill ON DELETE CASCADE,
+    cv_id int REFERENCES cv ON DELETE CASCADE,
+    skill_id int REFERENCES skill ON DELETE CASCADE,
     PRIMARY KEY (cv_id, skill_id)
 );
 
 CREATE TABLE vacancy_skill_assign (
-    vacancy_id uuid REFERENCES vacancy ON DELETE CASCADE,
-    skill_id uuid REFERENCES skill ON DELETE CASCADE,
+    vacancy_id int REFERENCES vacancy ON DELETE CASCADE,
+    skill_id int REFERENCES skill ON DELETE CASCADE,
     PRIMARY KEY (vacancy_id, skill_id)
 );
