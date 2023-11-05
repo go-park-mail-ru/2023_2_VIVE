301840a6d58a321be8da46011c0e254e92d35d96
Vladimir Borozenets <borozenets2002@mail.ru>
2023-11-05 12:42:20 +0300

    VIVE-57: Описать CRUD для отклика на вакансию

--- internal/repository/response.go
+++ internal/repository/response.go
@@ -1,5 +1,12 @@
 package repository
 
+import (
+	"HnH/pkg/serverErrors"
+
+	"database/sql"
+	"errors"
+)
+
 type IResponseRepository interface {
 	RespondToVacancy(vacancyID, cvID int) error
 	GetVacanciesIdsByCVId(cvID int) ([]int, error)
@@ -7,20 +14,68 @@ type IResponseRepository interface {
 }
 
 type psqlResponseRepository struct {
+	responseStorage *sql.DB
 }
 
-func NewPsqlResponseRepository() IResponseRepository {
-	return &psqlResponseRepository{}
+func NewPsqlResponseRepository(db *sql.DB) IResponseRepository {
+	return &psqlResponseRepository{
+		responseStorage: db,
+	}
 }
 
 func (p *psqlResponseRepository) RespondToVacancy(vacancyID, cvID int) error {
+	err := p.responseStorage.QueryRow(`INSERT INTO hnh_data.response ("vacancy_id", "cv_id") VALUES ($1, $2)`, vacancyID, cvID).Err()
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
 func (p *psqlResponseRepository) GetVacanciesIdsByCVId(cvID int) ([]int, error) {
-	return []int{}, nil
+	rows, err := p.responseStorage.Query(`SELECT vacancy_id FROM hnh_data.response WHERE cv_id = $1`, cvID)
+	if errors.Is(err, sql.ErrNoRows) {
+		return nil, serverErrors.NO_DATA_FOUND
+	} else if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	result := []int{}
+	for rows.Next() {
+		var vacancyID int
+
+		err := rows.Scan(&vacancyID)
+		if err != nil {
+			return nil, err
+		}
+		result = append(result, vacancyID)
+	}
+
+	return result, nil
 }
 
 func (p *psqlResponseRepository) GetAttachedCVs(vacancyID int) ([]int, error) {
-	return []int{}, nil
+	rows, err := p.responseStorage.Query(`SELECT cv_id FROM hnh_data.response WHERE vacancy_id = $1`, vacancyID)
+	if errors.Is(err, sql.ErrNoRows) {
+		return nil, serverErrors.NO_DATA_FOUND
+	} else if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	result := []int{}
+	for rows.Next() {
+		var cvID int
+
+		err := rows.Scan(&cvID)
+		if err != nil {
+			return nil, err
+		}
+		result = append(result, cvID)
+	}
+
+	return result, nil
 }
