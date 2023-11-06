c13439dad30d437956e2f87d769c56681c16508b
Vladimir Konoplyuk <konoplyuk.v@yandex.ru>
2023-11-06 06:10:08 +0300

    VIVE-55: Покрыть тестами функцию GetAllVacancies()

--- internal/repository/psql/psql_vacancy_test.go
+++ internal/repository/psql/psql_vacancy_test.go
@@ -11,11 +11,27 @@ import (
 	"gopkg.in/DATA-DOG/go-sqlmock.v1"
 )
 
+var vacanciesColumns = []string{
+	"id",
+	"employer_id",
+	"name",
+	"description",
+	"salary_lower_bound",
+	"salary_upper_bound",
+	"employment",
+	"experience_lower_bound",
+	"experience_upper_bound",
+	"education_type",
+	"location",
+	"created_at",
+	"updated_at",
+}
+
 var location, _ = time.LoadLocation("Local")
 var created_at = time.Date(2023, 11, 1, 0, 0, 0, 0, location)
 var updated_at = time.Date(2023, 11, 2, 0, 0, 0, 0, location)
 
-var rows = []struct {
+var testGetAllVacanciesSuccessCases = []struct {
 	expected []domain.Vacancy
 }{
 	{
@@ -82,22 +98,8 @@ func TestGetAllVacanciesSuccess(t *testing.T) {
 
 	repo := NewPsqlVacancyRepository(db)
 
-	for _, testCase := range rows {
-		rows := sqlmock.NewRows([]string{
-			"id",
-			"employer_id",
-			"name",
-			"description",
-			"salary_lower_bound",
-			"salary_upper_bound",
-			"employment",
-			"experience_lower_bound",
-			"experience_upper_bound",
-			"education_type",
-			"location",
-			"created_at",
-			"updated_at",
-		})
+	for _, testCase := range testGetAllVacanciesSuccessCases {
+		rows := sqlmock.NewRows(vacanciesColumns)
 
 		for _, item := range testCase.expected {
 			rows = rows.AddRow(
@@ -155,7 +157,33 @@ func TestGetAllVacanciesQueryError(t *testing.T) {
 		return
 	}
 	if queryErr == nil {
-		t.Errorf("expected error %s, got nil", err)
+		t.Errorf("expected query error, got nil")
+		return
+	}
+}
+
+func TestGetAllVacanciesEnriryNotFoundError(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
+
+	repo := NewPsqlVacancyRepository(db)
+
+	rows := sqlmock.NewRows(vacanciesColumns)
+
+	mock.
+		ExpectQuery("SELECT(.|\n)+FROM(.|\n)+").
+		WillReturnRows(rows)
+
+	_, returnedErr := repo.GetAllVacancies()
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+		return
+	}
+	if returnedErr == nil {
+		t.Errorf("expected error 'ErrEntityNotFound', got nil")
 		return
 	}
 }
