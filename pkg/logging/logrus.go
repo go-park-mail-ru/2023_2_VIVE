d44923ecaae8a065736ad878fe9291e848d952db
Vladimir Borozenets <borozenets2002@mail.ru>
2023-11-06 15:38:09 +0300

    VIVE-76: Изменить формат даты в логах

--- pkg/logging/logrus.go
+++ pkg/logging/logrus.go
@@ -8,7 +8,7 @@ import (
 
 func InitLogger(logFile *os.File) *logrus.Logger {
 	jsonFormatter := &logrus.JSONFormatter{
-		TimestampFormat: "2006-01-02 15:04:05",
+		TimestampFormat: "Mon, 02 Jan 2006 15:04:05 MST",
 	}
 
 	logger := logrus.New()
