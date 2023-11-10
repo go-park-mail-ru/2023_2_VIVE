2d04f8a640c22d9d86f879289cde967356c73a3d
Vladimir Konoplyuk <konoplyuk.v@yandex.ru>
2023-11-10 19:45:56 +0300

    Изменить конфиг

--- configs/server_configs.go
+++ configs/server_configs.go
@@ -16,7 +16,16 @@ const (
 )
 
 var CORS = cors.New(cors.Options{
-	AllowedOrigins:   []string{"http://212.233.90.231:8082", "http://212.233.90.231:8083", "http://212.233.90.231:8084", "http://212.233.90.231:8085"},
+	AllowedOrigins: []string{
+		"http://localhost:8082",
+		"http://localhost:8083",
+		"http://localhost:8084",
+		"http://localhost:8085",
+		// "http://212.233.90.231:8082",
+		// "http://212.233.90.231:8083",
+		// "http://212.233.90.231:8084",
+		// "http://212.233.90.231:8085",
+	},
 	AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE"},
 	AllowCredentials: true,
 })
