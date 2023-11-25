9d841e6aae0aea6cd8450180614ec5838317b2ea
Vladimir Konoplyuk <konoplyuk.v@yandex.ru>
2023-11-25 14:15:46 +0300

    VIVE-123: Добавить слой delivery

--- services/csat/internal/delivery/csat.go
+++ services/csat/internal/delivery/csat.go
@@ -4,6 +4,7 @@ import (
 	pb "HnH/services/csat/csatPB"
 	"HnH/services/csat/internal/repository/psql"
 	"HnH/services/csat/internal/usecase"
+	"context"
 	"database/sql"
 )
 
@@ -19,4 +20,16 @@ func NewServer(db *sql.DB) (*CsatServer, error) {
 	return &CsatServer{
 		csatUscase: csatUsecase,
 	}, nil
+}
+
+func (s *CsatServer) GetQuestions(ctx context.Context, userID *pb.UserID) (*pb.QuestionList, error) {
+	return s.csatUscase.GetQuestions(ctx, userID)
+}
+
+func (s *CsatServer) GetStatistic(ctx context.Context, empty *pb.Empty) (*pb.Statistics, error) {
+	return s.csatUscase.GetStatistics(ctx)
+}
+
+func (s *CsatServer) RegisterAnswer(ctx context.Context, answer *pb.Answer) (*pb.Empty, error) {
+	return s.csatUscase.RegisterAnswer(ctx, answer)
 }
