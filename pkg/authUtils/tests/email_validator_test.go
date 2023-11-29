c023f929c00084709fb32df2fc20e98a11c74c69
Vladimir Konoplyuk <konoplyuk.v@yandex.ru>
2023-11-30 01:20:55 +0300

    VIVE-127: Добавить тесты для authUtils

---
+++ pkg/authUtils/tests/email_validator_test.go
@@ -0,0 +1,66 @@
+package authUtils
+
+import (
+	"HnH/pkg/authUtils"
+	"HnH/pkg/testHelper"
+	"testing"
+)
+
+var testValidateEmailCases = []struct {
+	email       string
+	expectedErr error
+}{
+	{
+		email:       "email@example.com",
+		expectedErr: nil,
+	},
+	{
+		email:       "email@mail.ru",
+		expectedErr: nil,
+	},
+	{
+		email:       "email@yandex.ru",
+		expectedErr: nil,
+	},
+	{
+		email:       "email@google.ru",
+		expectedErr: nil,
+	},
+	{
+		email:       "email@google.com",
+		expectedErr: nil,
+	},
+	// {
+	// 	email:       "email@google.co",
+	// 	expectedErr: authUtils.INVALID_EMAIL,
+	// },
+	{
+		email:       "email@mail",
+		expectedErr: authUtils.INVALID_EMAIL,
+	},
+	{
+		email:       "email mail.ru",
+		expectedErr: authUtils.INVALID_EMAIL,
+	},
+	{
+		email:       "mail.ru",
+		expectedErr: authUtils.INVALID_EMAIL,
+	},
+	{
+		email:       "mail.ru",
+		expectedErr: authUtils.INVALID_EMAIL,
+	},
+	{
+		email:       "",
+		expectedErr: authUtils.EMPTY_EMAIL,
+	},
+}
+
+func TestValidateEmail(t *testing.T) {
+	for _, testCase := range testValidateEmailCases {
+		actualErr := authUtils.ValidateEmail(testCase.email)
+		if actualErr != testCase.expectedErr {
+			t.Errorf(testHelper.ErrNotEqual(testCase.expectedErr, actualErr))
+		}
+	}
+}
